9328ae60aa77d9b8dd37fec42e8f4a0e8b0e8a36
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-04-04 11:54:18 +0200

    Next.js setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="mt-10 animate-bounce content-center text-center text-lg">
+        Hello world
+      </p>
+    </div>
+  );
+}
