e1f38f3855f088ace3525b6e32b8ce5dd67ed61d
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-04-04 13:54:34 +0200

    bare bones structure

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <p className="mt-10 animate-bounce content-center text-center text-lg">
+        Hello Settings!
+      </p>
+    </div>
+  );
+}
