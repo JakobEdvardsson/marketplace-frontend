000de960b7d8e42b0fee8083296e4646946e8834
Filip Bengtegård <bengtegardbook@gmail.com>
2024-04-11 13:29:49 +0200

    fix prettier issues

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,28 +1,34 @@
-'use client'
+"use client";
 import Link from "next/link";
 // import {router} from "next/client";
 export default function Navbar() {
+  const navLinks = [
+    { name: "Home", link: "/" },
+    { name: "Search", link: "/search" },
+    { name: "Shopping cart", link: "/cart" },
+    { name: "Profile", link: "/profile" },
+    { name: "Settings", link: "/settings" },
+  ];
 
-    const navLinks = [
-        {name: "Home", link: "/"},
-        {name: "Search", link: "/search"},
-        {name: "Shopping cart", link: "/cart"},
-        {name: "Profile", link: "/profile"},
-        {name: "Settings", link: "/settings"}
-    ]
+  const renderLinks = () => (
+    <ul
+      id="navbar"
+      className="fixed top-0 flex h-10 w-full flex-row items-center justify-around lg:h-[5%]"
+    >
+      {navLinks.map((link) => (
+        <li
+          key={link.name}
+          className="w-fit text-center sm:duration-300 sm:ease-in-out lg:hover:scale-110"
+        >
+          <Link href={link.link}>
+            <button type="button" className="text-xl lg:text-3xl">
+              {link.name}
+            </button>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 
-    const renderLinks = () => (
-            <ul id="navbar" className="fixed top-0 flex h-10 w-full flex-row items-center justify-around lg:h-[5%]">
-                {navLinks.map((link) => (
-                        <li key={link.name}
-                            className="w-fit text-center sm:duration-300 sm:ease-in-out lg:hover:scale-110">
-                            <Link href={link.link} >
-                                <button type="button" className="text-xl lg:text-3xl">{link.name}</button>
-                            </Link>
-                        </li>
-                    ))}
-            </ul>
-        )
-
-    return renderLinks();
+  return renderLinks();
 }
