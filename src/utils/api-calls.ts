32b389ed6196f305a2886f92b6ac25ee682f68d7
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-09 03:15:05 +0200

    Create api-calls.ts

---
+++ src/utils/api-calls.ts
@@ -0,0 +1,3 @@
+const BACKEND_HOST = process.env.BACKEND_URL || "http://localhost:8080";
+const BACKEND_API_VERSION = process.env.BACKEND_API_VERSION || "v1";
+const BACKEND_URL = `${BACKEND_HOST}/${BACKEND_API_VERSION}`;
