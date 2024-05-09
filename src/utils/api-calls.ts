2594dbc4991d6cc7ed37ba2b9432cf6d1f89d60c
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-09 03:15:41 +0200

    Api-calls: Add login endpoint

--- src/utils/api-calls.ts
+++ src/utils/api-calls.ts
@@ -1,3 +1,16 @@
 const BACKEND_HOST = process.env.BACKEND_URL || "http://localhost:8080";
 const BACKEND_API_VERSION = process.env.BACKEND_API_VERSION || "v1";
 const BACKEND_URL = `${BACKEND_HOST}/${BACKEND_API_VERSION}`;
+
+export function login(username: string, password: string) {
+  const url = `${BACKEND_URL}/accounts/login`;
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    credentials: "include",
+    body: `username=${username}&password=${password}`,
+  });
+}
