e15daf626e3208e5cb81ab798f3001ab083c2926
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-05-12 12:45:50 +0200

    LoginForm added to login package and separated as element

---
+++ src/app/(no-nav)/login/LoginForm.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function LoginForm() {
+  const handleSubmit = async (formData: FormData) => {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    // await login(username, password).then(res => {...});
+  };
+
+  return (
+    <form action={handleSubmit}>
+      <input required type="text" name="username" placeholder="Username" />
+      <input required type="password" name="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
--- src/app/(no-nav)/login/page.tsx
+++ src/app/(no-nav)/login/page.tsx
@@ -1,50 +1,12 @@
-"use client";
-import { useRouter } from "next/navigation";
+import LoginForm from "@/app/(no-nav)/login/LoginForm";
+import Link from "next/link";
 
 export default function Page() {
-  const router = useRouter();
-
-  const handleSubmit = async (formData: FormData) => {
-    const email = formData.get("email");
-    const password = formData.get("password");
-
-    //TODO: connect API here
-    /*
-            const response = await();
-
-            if (response.ok) {
-                router.push('/');
-            } else {
-                // TODO: handle errors here
-            }
-             */
-
-    //TODO: remove this lines
-    router.push("/");
-    console.log(email, password);
-  };
-
   return (
-    <form
-      className="flex h-screen w-screen bg-gray-50 align-middle"
-      action={handleSubmit}
-    >
-      <div className="m-auto flex size-2/4 flex-col items-center justify-around rounded-xl bg-blue-600 drop-shadow-xl">
-        <h1 className="bg-red-400 text-center text-2xl">
-          Welcome to Marketplace
-        </h1>
-        <input required type="email" name="email" placeholder="Email" />
-        <input
-          required
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-        <button type="submit">Login</button>
-        <p>
-          Dont have an account? <a href="/register">Register here!</a>
-        </p>
-      </div>
-    </form>
+    <>
+      <p>Login to the best marketplace in the world</p>
+      <LoginForm />
+      <Link href="/register">No account? Register here!</Link>
+    </>
   );
 }
