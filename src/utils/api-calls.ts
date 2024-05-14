1ab6154f79189301e24a6a86b8ab5869d717ecad
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-14 12:28:00 +0200

    Fix environment variables for production
    Environment variable that must be available in the client's browser must be prefixed with "NEXT_PUBLIC" otherwise they will only be available on the frontend server

--- src/utils/api-calls.ts
+++ src/utils/api-calls.ts
@@ -3,8 +3,9 @@
 // TODO: add more query params for "search" endpoints + pagination?
 // TODO: add types to all endpoints
 
-const BACKEND_HOST = process.env.BACKEND_URL || "http://localhost:8080";
-const BACKEND_API_VERSION = process.env.BACKEND_API_VERSION || "v1";
+const BACKEND_HOST =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
+const BACKEND_API_VERSION = process.env.NEXT_PUBLIC_BACKEND_API_VERSION || "v1";
 const BACKEND_URL = `${BACKEND_HOST}/${BACKEND_API_VERSION}`;
 
 /*
