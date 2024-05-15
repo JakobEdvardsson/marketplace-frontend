ac30ba16114736d8f259737e17603355624e607d
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-05-15 11:30:34 +0200

    Register page developed with post request
    - Animations added to tailwind
    - Register page and form developed
    - registration function is developed with poor error handling
    -maximum length of username set to 18

--- src/app/(no-nav)/register/page.tsx
+++ src/app/(no-nav)/register/page.tsx
@@ -1,10 +1,14 @@
+import RegisterForm from "@/app/(no-nav)/register/RegisterForm";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <div>
-      <p className="mt-10 animate-bounce content-center text-center text-lg">
-        Hello register!
-        <a href="/login">Go to Login</a>
-      </p>
+    <div className=" flex h-screen w-screen items-center bg-gray-100 align-middle">
+      <div className="relative m-auto flex w-3/4">
+        <div className="mx-auto w-2/5">
+          <RegisterForm />
+        </div>
+      </div>
     </div>
   );
 }
