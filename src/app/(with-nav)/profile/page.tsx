a826f61c3385090a640d1c53bdd4bcc6e395aab0
Filip Bengtegård <bengtegardbook@gmail.com>
2024-05-16 18:19:12 +0200

    initial commit

--- src/app/(with-nav)/profile/page.tsx
+++ src/app/(with-nav)/profile/page.tsx
@@ -1,4 +1,4 @@
-export default function Page() {
+export default function Profile() {
   return (
     <div className="w-full bg-yellow-300">
       <p className="mt-10 animate-pulse content-center text-center text-3xl">
