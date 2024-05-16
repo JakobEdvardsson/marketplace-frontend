c317c95cfa04376d95fe7c0cbce2f14e9ea9b026
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-16 23:03:30 +0200

    Fix root layout issues

--- src/app/(no-nav)/layout.tsx
+++ src/app/(no-nav)/layout.tsx
@@ -1,21 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../(with-nav)/globals.css";
 import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Marketplace",
-  description: "Marketplace",
-};
-
-export default function RootLayout({
+export default function Layout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+}: {
+  readonly children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Marketplace",
+  description: "Marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
