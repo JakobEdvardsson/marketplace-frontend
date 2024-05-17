c70dc8fbe50e9446b8787d4b4f899815f79b333e
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-17 20:22:15 +0200

    Add cart v1
    - Added cart context provider
    - Added new cart type
    - Added component to test adding to cart
    - Added component to see cart items

---
+++ src/app/(with-nav)/cart/components/ShoppingCart.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import { useCart } from "@/components/CartContext";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export function ShoppingCart() {
+  const { items, removeFromCart } = useCart();
+
+  return (
+    <div className="flex justify-center">
+      {items.length === 0 && (
+        <p className="text-gray-600">Your cart is empty.</p>
+      )}
+      <ul>
+        {items.map((item) => (
+          <li
+            key={item.productId}
+            className="flex items-center justify-between border-b border-gray-200 py-4"
+          >
+            <div className="flex items-center">
+              <div className="mr-4">
+                <Image
+                  src={item.imageUrls[0]}
+                  alt={item.name}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div>
+                <div className="font-bold">{item.name}</div>
+                <div className="text-gray-600">
+                  Category: {item.productCategory.name}
+                </div>
+                <div className="text-gray-600">Price: ${item.price}</div>
+                <div className="text-gray-600">
+                  Description: {item.description}
+                </div>
+                <div className="text-gray-600">Condition: {item.condition}</div>
+                <div className="text-gray-600">Status: {item.status}</div>
+                <div className="text-gray-600">Seller: {item.seller}</div>
+                <div className="text-gray-600">Buyer: {item.buyer}</div>
+                <div className="text-gray-600">
+                  Color: {item.color || "N/A"}
+                </div>
+                <div className="text-gray-600">
+                  Production Year: {item.productionYear || "N/A"}
+                </div>
+                <div className="text-gray-600">
+                  Created At: {new Date(item.createdAt).toLocaleString()}
+                </div>
+              </div>
+            </div>
+            <div>
+              <Button onClick={() => removeFromCart(item.productId)}>
+                Remove
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
