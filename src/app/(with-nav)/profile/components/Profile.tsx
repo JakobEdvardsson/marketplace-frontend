29eaf96bfe232c919924435c5607215ce12d6a89
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-05-18 21:30:54 +0200

    Fix links and make navbar change depending on login-state (#22)
    * Add navbar state change based on login-status
    
    * Remove unnecessary useCallback from auth context
    
    * Fix settings link in MyMarketplace
    
    * Auth check on profile
    
    * Make navbar's "Marketplace" text clickable & add svg profile picture

--- src/app/(with-nav)/profile/components/Profile.tsx
+++ src/app/(with-nav)/profile/components/Profile.tsx
@@ -5,13 +5,19 @@ import { useEffect, useState } from "react";
 import { MyProfileResponseDTO } from "@/types/endpoint-types-incoming";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useAuth } from "@/components/AuthContext";
 
 export default function MyProfile() {
   const router = useRouter();
+  const auth = useAuth();
 
   const [profile, setProfile] = useState<MyProfileResponseDTO | undefined>();
 
   useEffect(() => {
+    if (!auth.loggedIn) {
+      router.push("/login");
+    }
+
     getMyProfile()
       .then((response) => {
         if (response.status === 401) {
@@ -23,7 +29,7 @@ export default function MyProfile() {
         });
       })
       .catch((e) => console.error(e));
-  }, [router]);
+  }, [auth.loggedIn, router]);
 
   return (
     <div className="w-full">
