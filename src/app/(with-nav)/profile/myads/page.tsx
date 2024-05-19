7eef0e288482b1c0ed0fcbdb603c11ad2bf3b0cd
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2024-05-19 20:03:35 +0200

    My active products page is developed

---
+++ src/app/(with-nav)/profile/myads/page.tsx
@@ -0,0 +1,9 @@
+import MyProducts from "@/app/(with-nav)/profile/myads/MyProducts";
+
+export default function Page() {
+  return (
+    <div className="size-full bg-gray-100 py-10">
+      <MyProducts />
+    </div>
+  );
+}
