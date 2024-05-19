ed2d6c747f4c233f90c659e36fb114b62d923a7a
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-19 21:13:39 +0200

    Add checkout functionality

--- src/app/(with-nav)/cart/page.tsx
+++ src/app/(with-nav)/cart/page.tsx
@@ -1,10 +1,12 @@
 import { ShoppingCart } from "@/app/(with-nav)/cart/components/ShoppingCart";
+import Checkout from "@/app/(with-nav)/cart/components/Checkout";
+import { LatestProducts } from "@/app/(with-nav)/cart/components/LatestProducts";
 
 export default function Page() {
   return (
-    <>
+    <div className="flex flex-col items-center">
+      <Checkout />
       <ShoppingCart />
-      {/*<TopProducts /> */}
-    </>
+    </div>
   );
 }
