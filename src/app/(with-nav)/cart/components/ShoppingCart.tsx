aaaaed38906d68a6f119c03a355959fa6ee5ccef
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-05-19 21:10:51 +0200

    Tweak ShoppingCart CSS

--- src/app/(with-nav)/cart/components/ShoppingCart.tsx
+++ src/app/(with-nav)/cart/components/ShoppingCart.tsx
@@ -11,7 +11,7 @@ export function ShoppingCart() {
   return (
     <div className="flex justify-center">
       {items.length === 0 && (
-        <p className="text-gray-600">Your cart is empty.</p>
+        <p className="mb-5 mt-10 text-center text-2xl text-gray-600">Your cart is empty.</p>
       )}
       <ul>
         {items.map((item) => (
