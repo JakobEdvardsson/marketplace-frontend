17f7d7cba3b42dfa0e1f6a23b347ce9b3e934402
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-05-20 13:32:35 +0200

    lint issues

--- src/app/(with-nav)/profile/myads/MyAds.tsx
+++ src/app/(with-nav)/profile/myads/MyAds.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-
 import { deleteProductById, handleBuyOrderRequest } from "@/utils/api-calls";
 import {
   mutateMyActiveListings,
-  useMyActiveListings
+  useMyActiveListings,
 } from "@/utils/api-calls-swr";
 import { useToast } from "@/components/ui/use-toast";
 import Link from "next/link";
@@ -16,13 +15,12 @@ export default function MyAds() {
 
   const { data: products } = useMyActiveListings();
 
-
   const setProductStatus = (id: string, status: boolean) => {
     handleBuyOrderRequest(id, status).then((res) => {
       if (res.ok) {
         toast({
           title: "Purchase request handled.",
-          variant: "default"
+          variant: "default",
         });
         mutateMyActiveListings();
       }
@@ -45,7 +43,8 @@ export default function MyAds() {
     products.activeListings.map((product) => (
       <div
         key={product.id}
-        className=" my-3 flex flex-col items-center justify-between rounded-lg bg-white p-5 shadow-md">
+        className=" my-3 flex flex-col items-center justify-between rounded-lg bg-white p-5 shadow-md"
+      >
         <div className="flex w-full items-center justify-between">
           <h1>{product.productName}</h1>
           <p>{product.productCategoryName}</p>
@@ -58,41 +57,47 @@ export default function MyAds() {
             Delete
           </button>
         </div>
-        {product.productStatus === 1 ?
+        {product.productStatus === 1 ? (
           <div className="my-3 flex flex-col items-center">
             <p className="mt-2 font-bold">Purchase request:</p>
             <div>
-              <Button className="m-2 bg-green-500 hover:bg-green-600" type="button" onClick={()=>{setProductStatus(product.id,true)}}>Accept</Button>
-              <Button className="m-2 bg-red-500 hover:bg-red-800" type="button" onClick={()=>{setProductStatus(product.id,false)}}>Deny</Button>
+              <Button
+                className="m-2 bg-green-500 hover:bg-green-600"
+                type="button"
+                onClick={() => {
+                  setProductStatus(product.id, true);
+                }}
+              >
+                Accept
+              </Button>
+              <Button
+                className="m-2 bg-red-500 hover:bg-red-800"
+                type="button"
+                onClick={() => {
+                  setProductStatus(product.id, false);
+                }}
+              >
+                Deny
+              </Button>
             </div>
-
           </div>
-          : null}
-
+        ) : null}
       </div>
-
     ))
   ) : (
     <div>
       <h1 className="text-2xl font-bold">Loading...</h1>
 
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div
-        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
     </div>
   );
 
   return (
-
     <div className="mt-3 w-8/12 px-2">
       <div className="flex space-x-2 ">
         <Link className="text-sm hover:underline" href="/profile">
