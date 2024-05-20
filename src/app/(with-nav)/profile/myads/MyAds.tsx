ef8bc1692543e1001d1d33cd830c5998584c2b6b
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-05-20 13:23:16 +0200

    Implement Accept / Deny after Purchase request

--- src/app/(with-nav)/profile/myads/MyAds.tsx
+++ src/app/(with-nav)/profile/myads/MyAds.tsx
@@ -1,16 +1,33 @@
 "use client";
 
-import { deleteProductById } from "@/utils/api-calls";
+
+import { deleteProductById, handleBuyOrderRequest } from "@/utils/api-calls";
 import {
   mutateMyActiveListings,
-  useMyActiveListings,
+  useMyActiveListings
 } from "@/utils/api-calls-swr";
 import { useToast } from "@/components/ui/use-toast";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function MyAds() {
   const { toast } = useToast();
 
   const { data: products } = useMyActiveListings();
+
+
+  const setProductStatus = (id: string, status: boolean) => {
+    handleBuyOrderRequest(id, status).then((res) => {
+      if (res.ok) {
+        toast({
+          title: "Purchase request handled.",
+          variant: "default"
+        });
+        mutateMyActiveListings();
+      }
+    });
+  };
 
   const deleteProduct = (id: string) => {
     deleteProductById(id).then((res) => {
@@ -28,35 +45,64 @@ export default function MyAds() {
     products.activeListings.map((product) => (
       <div
         key={product.id}
-        className=" my-3 flex h-24 flex-row items-center justify-between rounded-lg bg-white p-5 shadow-md"
-      >
-        <h1>{product.productName}</h1>
-        <p>{product.productCategoryName}</p>
-        <p>{product.price + " kr"}</p>
-        <button
-          className="cursor-help font-bold text-red-700"
-          type="button"
-          onClick={() => deleteProduct(product.id)}
-        >
-          Delete
-        </button>
+        className=" my-3 flex flex-col items-center justify-between rounded-lg bg-white p-5 shadow-md">
+        <div className="flex w-full items-center justify-between">
+          <h1>{product.productName}</h1>
+          <p>{product.productCategoryName}</p>
+          <p>{product.price + " kr"}</p>
+          <button
+            className="cursor-help font-bold text-red-700"
+            type="button"
+            onClick={() => deleteProduct(product.id)}
+          >
+            Delete
+          </button>
+        </div>
+        {product.productStatus === 1 ?
+          <div className="my-3 flex flex-col items-center">
+            <p className="mt-2 font-bold">Purchase request:</p>
+            <div>
+              <Button className="m-2 bg-green-500 hover:bg-green-600" type="button" onClick={()=>{setProductStatus(product.id,true)}}>Accept</Button>
+              <Button className="m-2 bg-red-500 hover:bg-red-800" type="button" onClick={()=>{setProductStatus(product.id,false)}}>Deny</Button>
+            </div>
+
+          </div>
+          : null}
+
       </div>
+
     ))
   ) : (
     <div>
       <h1 className="text-2xl font-bold">Loading...</h1>
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
-      <div className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
+      <div
+        className=" my-3 flex h-24 animate-pulse flex-row items-center justify-between rounded-lg bg-gray-300 p-5 shadow-md" />
     </div>
   );
 
   return (
-    <div className="size-full p-10">
-      <h1 className="text-4xl font-bold">My Products</h1>
+
+    <div className="mt-3 w-8/12 px-2">
+      <div className="flex space-x-2 ">
+        <Link className="text-sm hover:underline" href="/profile">
+          My Marketplace
+        </Link>
+        <Image src="/images/arrow.svg" alt="arrow" width="8" height="8" />
+        <p className="text-sm text-gray-500">My Ads</p>
+      </div>
+
+      <h1 className="my-5 text-4xl font-bold">My Ads</h1>
       {unpackedProducts}
     </div>
   );
--- src/app/(with-nav)/profile/myads/page.tsx
+++ src/app/(with-nav)/profile/myads/page.tsx
@@ -2,7 +2,7 @@ import MyAds from "@/app/(with-nav)/profile/myads/MyAds";
 
 export default function Page() {
   return (
-    <div className="size-full bg-gray-100 py-10">
+    <div className="flex size-full justify-center bg-gray-100">
       <MyAds />
     </div>
   );
