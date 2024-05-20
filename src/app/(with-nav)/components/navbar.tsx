ba9032a8d27d4242e4089909eb43b4a03f225d8c
Filip Bengtegård <bengtegardbook@gmail.com>
2024-05-20 16:20:50 +0200

    redesigned navbar to look like blocket, also rename marketplace to: PLOCKET

--- src/app/(with-nav)/components/navbar.tsx
+++ src/app/(with-nav)/components/navbar.tsx
@@ -2,21 +2,14 @@
 
 import Link from "next/link";
 import { useAuth } from "@/components/AuthContext";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useTestAuth } from "@/utils/api-calls-swr";
 
 export default function Navbar() {
   const { loggedIn } = useAuth();
+  const { data: username } = useTestAuth();
 
-  const authenticatedNavLinks = [
-    { name: "Post ad", link: "/product" },
-    { name: "Shopping cart", link: "/cart" },
-  ];
-
-  const anonymousNavLinks = [{ name: "Login", link: "/login" }];
-
-  const navLinks = loggedIn ? authenticatedNavLinks : anonymousNavLinks;
   const router = useRouter();
   const handleClickLogin = () => {
     router.push("/login");
@@ -34,7 +27,11 @@ export default function Navbar() {
     router.push("/");
   };
 
-  const renderLinks2 = () =>
+  const handleClickProfile = () => {
+    router.push("/profile");
+  };
+
+  const renderLinks = () =>
     loggedIn ? (
       <div className="flex h-14 w-full bg-white shadow-md drop-shadow-md">
         <div className="mx-auto flex w-8/12 flex-row items-center justify-between">
@@ -105,62 +102,44 @@ export default function Navbar() {
               </svg>
               Cart
             </Button>
-            <Link href="/profile" className="">
-              <Image
-                src="/images/default-profile-picture.svg"
-                alt="Default Profile"
-                width={50}
-                height={50}
-              />
-            </Link>
+            <Button
+              variant="link"
+              className="mr-4 border-white"
+              onClick={handleClickProfile}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+              {username?.username ? username.username : null}
+            </Button>
           </div>
         </div>
       </div>
     ) : (
-      <div className="flex h-14 w-full items-center bg-white shadow-md drop-shadow-md">
-        <div className="mx-auto w-8/12">
-          <Link className="font-black text-red-600 lg:text-3xl" href="/">
-            Marketplace
+      <div className="flex h-14 w-full bg-white shadow-md drop-shadow-md">
+        <div className="mx-auto flex w-8/12 flex-row items-center justify-between">
+          <Link className="ml-6 font-black text-red-600 lg:text-3xl" href="/">
+            Plocket
           </Link>
-          <Button className="ml-96" onClick={handleClickLogin}>
-            Login
-          </Button>
+          <div className="flex items-center justify-end">
+            <Button className="mr-8" onClick={handleClickLogin}>
+              Login
+            </Button>
+          </div>
         </div>
       </div>
     );
 
-  const renderLinks = () => (
-    <ul
-      id="navbar"
-      className="flex h-14 w-full flex-row items-center justify-around bg-white shadow-md drop-shadow-md"
-    >
-      <li className="font-black text-red-600 lg:mr-60 lg:text-3xl">
-        <Link href="/">Marketplace</Link>
-      </li>
-      {navLinks.map((link) => (
-        <li
-          key={link.name}
-          className="w-fit text-center text-gray-500 hover:border-b-2 hover:border-b-black hover:text-gray-600 sm:duration-100 sm:ease-in-out "
-        >
-          <button type="button" className="text-xs lg:text-xl ">
-            <Link href={link.link}>{link.name}</Link>
-          </button>
-        </li>
-      ))}
-      {loggedIn && (
-        <li key="avatar" className="w-fit text-center text-gray-500">
-          <Link href="/profile">
-            <Image
-              src="/images/default-profile-picture.svg"
-              alt="Default Profile"
-              width={50}
-              height={50}
-            />
-          </Link>
-        </li>
-      )}
-    </ul>
-  );
-
-  return renderLinks2();
+  return renderLinks();
 }
