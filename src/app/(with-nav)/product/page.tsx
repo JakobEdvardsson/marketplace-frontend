e02d5aaf0307bbaffa5124a875ef00e2d908fcb7
Filip Bengtegård <bengtegardbook@gmail.com>
2024-06-10 14:06:56 +0200

    add same font size when you are not logged in.

--- src/app/(with-nav)/product/page.tsx
+++ src/app/(with-nav)/product/page.tsx
@@ -2,7 +2,7 @@ import CreateProduct from "@/app/(with-nav)/product/CreateProduct";
 
 export default function Page() {
   return (
-    <div className="size-full bg-gray-100 py-10">
+    <div className="py-10">
       <CreateProduct />
     </div>
   );
