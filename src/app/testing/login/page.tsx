2bf35e1c344de917513c7a8dfbffd1e7596e7af3
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-10 18:07:50 +0200

    Delete testing pages (#67)

--- src/app/testing/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestEndpoint from "@/app/testing/components/TestEndpoint";
-
-export default function Page() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <TestEndpoint />
-    </div>
-  );
-}
