56a196b842efd9a86666d22a70df12c8ce035d52
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-11 19:44:35 +0200

    Update page.tsx

--- src/app/(with-nav)/product/[productId]/page.tsx
+++ src/app/(with-nav)/product/[productId]/page.tsx
@@ -6,7 +6,7 @@ export default function Page({
   readonly params: { productId: string };
 }) {
   return (
-    <div className="mx-auto my-3 flex w-11/12 justify-center sm:px-0 2md:w-8/12">
+    <div className="mx-auto my-3 flex w-11/12 justify-center 2md:w-8/12">
       <Product id={params.productId} />
     </div>
   );
