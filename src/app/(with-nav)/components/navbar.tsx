5b1184660db4a2339caf6ca8d486b53cc2620259
Filip Bengtegård <bengtegardbook@gmail.com>
2024-06-11 19:46:17 +0200

    add cart icon when adding products to cart

--- src/app/(with-nav)/components/navbar.tsx
+++ src/app/(with-nav)/components/navbar.tsx
@@ -6,8 +6,10 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { testAuth } from "@/utils/api-calls";
 import { useState } from "react";
+import { useCart } from "@/components/CartContext";
 
 export default function Navbar() {
+  const { items } = useCart();
   const { loggedIn } = useAuth();
   const [username, setUsername] = useState("");
 
@@ -120,22 +122,43 @@ export default function Navbar() {
             className="border-white mobile-br:flex-col mobile-br:py-0 mobile-br:text-xs"
             onClick={handleClickCart}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
+            <div className="relative inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+              {items.length ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="red"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="red"
+                  className="absolute -top-2 left-3 size-3"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+                  />
+                </svg>
+              ) : (
+                ""
+              )}
+            </div>
             Cart
           </Button>
+
           <Button
             variant="link"
             className="border-white pr-0 mobile-br:flex-col mobile-br:py-0 mobile-br:text-xs"
