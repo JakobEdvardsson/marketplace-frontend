db986c80c25d4f5d4d1472f0a9d0733cab9f5014
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-11 19:42:33 +0200

    Actually adjust product page CSS
    Product page width now follows navbar width

--- src/app/(with-nav)/product/[productId]/page.tsx
+++ src/app/(with-nav)/product/[productId]/page.tsx
@@ -6,7 +6,7 @@ export default function Page({
   readonly params: { productId: string };
 }) {
   return (
-    <div className="my-3 flex w-full justify-center px-4 sm:mx-auto sm:w-11/12 md:w-9/12 lg:w-8/12">
+    <div className="mx-auto my-3 flex w-11/12 justify-center sm:px-0 2md:w-8/12">
       <Product id={params.productId} />
     </div>
   );
