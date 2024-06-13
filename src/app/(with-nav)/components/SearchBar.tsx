67abbbe16ff90044b5202cb3fbe05db8491fab3d
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-13 18:41:23 +0200

    Use URL parameter to store search query

--- src/app/(with-nav)/components/SearchBar.tsx
+++ src/app/(with-nav)/components/SearchBar.tsx
@@ -1,21 +1,64 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { FormEvent, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchBar(props: {
   readonly handleSearch: (_: string) => void;
-  readonly query: string | undefined;
 }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParam = searchParams.get("q");
+
+  useEffect(() => {
+    if (searchParam) {
+      handleSearch(searchParam);
+    } else {
+      handleSearch("");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParam]);
+
   const handleSearch = (e: string) => {
     props.handleSearch(e);
   };
 
   return (
-    <form className="m-1 mb-2 text-center">
-      <Input
-        type="text"
-        placeholder="Search for products..."
-        value={props.query}
-        onChange={(e) => handleSearch(e.target.value)}
-      />
+    <form
+      className="m-1 mb-2"
+      onSubmit={(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const query = formData.get("query");
+        const params = new URLSearchParams(searchParams);
+        if (query instanceof File) {
+          return;
+        }
+
+        if (query) {
+          params.set("q", query);
+        } else {
+          params.delete("q");
+        }
+
+        replace(`${pathname}?${params.toString()}`);
+      }}
+    >
+      <div className="flex">
+        <Input
+          defaultValue={searchParam ? searchParam : undefined}
+          className=""
+          name="query"
+          type="search"
+          placeholder="Search for products..."
+        />
+        <Button type="submit" variant="blue" className="rounded-l-none">
+          Search
+        </Button>
+      </div>
     </form>
   );
 }
