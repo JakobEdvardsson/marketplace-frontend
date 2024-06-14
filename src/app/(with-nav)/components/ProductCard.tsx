de80a9699e69811e30ae55db942db3978ee06d25
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-14 05:02:17 +0200

    New product card v1

--- src/app/(with-nav)/components/ProductCard.tsx
+++ src/app/(with-nav)/components/ProductCard.tsx
@@ -1,4 +1,3 @@
-import { ProductCondition } from "@/utils/api-call-types";
 import { ProductGetResponseDTO } from "@/types/endpoint-types-incoming";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,10 +10,9 @@ export default function ProductCard(props: {
 
   return (
     <Link
-      className="group flex h-96 w-11/12 flex-col items-center border-b py-4 last:border-b-0 sm:h-48 sm:flex-row 2md:w-8/12"
+      className="group flex h-96 w-full flex-col sm:h-48 sm:flex-row"
       href={`/product/${product.productId}`}
     >
-      {/*Image*/}
       <Image
         src={product.imageUrls[0] || "/images/emptyImage.jpg"}
         className="h-2/3 w-full rounded bg-gray-200 object-contain sm:h-full sm:w-2/5"
@@ -22,24 +20,11 @@ export default function ProductCard(props: {
         width={1000}
         height={1000}
       />
-
-      {/*Description*/}
-      <div className="mt-2 flex h-auto w-full flex-col justify-around p-3 sm:mt-0 sm:w-3/5">
-        <div>
-          <h1 className="truncate group-hover:underline">{product.name}</h1>
-          <p>
-            {ProductCondition[product.condition]
-              .replace(/_/g, " ")
-              .toLowerCase()
-              .replace(/\b\w/g, (char) => char.toUpperCase())}
-          </p>
-          {product.productionYear ? (
-            <p>Year: {product.productionYear}</p>
-          ) : null}
-          <b>{product.price} kr</b>
-        </div>
-
-        <div className="flex h-full flex-row flex-wrap items-end justify-between align-top">
+      <div className="mt-2 flex h-auto w-full flex-col sm:mt-0 sm:w-3/5 sm:pl-3">
+        <div className="flex justify-between">
+          <a href="#">
+            <h1>{product.productCategory.name}</h1>
+          </a>
           <p>
             {createdAt.getDate() +
               "/" +
@@ -49,6 +34,16 @@ export default function ProductCard(props: {
               ":" +
               createdAt.getMinutes()}
           </p>
+        </div>
+
+        <div className="group-hover:underline">
+          <h1>{product.name}</h1>
+        </div>
+
+        <div className="flex grow flex-row" />
+
+        <div className="flex justify-between">
+          <p className="text-lg font-medium">{product.price} kr</p>
         </div>
       </div>
     </Link>
