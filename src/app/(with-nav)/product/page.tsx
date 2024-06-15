b36e9646760ed290ab1680376df641d3949d40fe
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-15 17:58:11 +0200

    Add missing page titles

--- src/app/(with-nav)/product/page.tsx
+++ src/app/(with-nav)/product/page.tsx
@@ -1,5 +1,11 @@
 import CreateProduct from "@/app/(with-nav)/product/components/CreateProduct";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Post ad | Marketplace",
+  description: "Marketplace - post ad",
+};
 
 export default function Page() {
   return <CreateProduct />;
