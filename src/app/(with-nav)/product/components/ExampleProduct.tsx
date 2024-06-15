811c49fc5630dc96df72eb69449a01dd69e8614e
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-15 06:17:29 +0200

    Bugfixes & currency formatting in ad preview

--- src/app/(with-nav)/product/components/ExampleProduct.tsx
+++ src/app/(with-nav)/product/components/ExampleProduct.tsx
@@ -2,16 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { ProductColor, ProductCondition } from "@/utils/api-call-types";
 
-export default function ExampleProduct({
-  files,
-  name,
-  price,
-  category,
-  condition,
-  description,
-  color,
-  year,
-}: {
+export default function ExampleProduct(props: {
   readonly files: File[] | undefined;
   readonly name: string;
   readonly price: number | undefined;
@@ -21,9 +12,15 @@ export default function ExampleProduct({
   readonly color: number | undefined;
   readonly year: number | undefined;
 }) {
+  const currencyFormat = new Intl.NumberFormat("sv-SE", {
+    style: "currency",
+    currency: "SEK",
+    maximumFractionDigits: 0,
+  });
+
   const renderFiles = () => {
-    if (files && files.length > 0) {
-      return files.map((file: File) => {
+    if (props.files && props.files.length > 0) {
+      return props.files.map((file: File) => {
         const objectUrl = URL.createObjectURL(file);
         return (
           <div key={file.name} className="mx-auto h-72 w-full shrink-0 pr-2">
@@ -51,40 +48,52 @@ export default function ExampleProduct({
     <div className="ml-2 w-full rounded-xl bg-white p-2 product-form-br:ml-0">
       <div className="mx-auto flex overflow-x-auto">{renderFiles()}</div>
       <h1
-        className={`my-3 cursor-default text-2xl font-bold mobile-br:text-lg ${name ? "" : "text-gray-300"}`}
+        className={`my-3 cursor-default text-2xl font-bold mobile-br:text-lg ${props.name ? "" : "text-gray-300"}`}
       >
-        {name === "" ? "No name" : name}
+        {props.name === "" ? "No name" : props.name}
       </h1>
 
       <h1
-        className={`cursor-default text-sm text-gray-400 ${category ? "" : "text-gray-300"}`}
+        className={`cursor-default text-sm text-gray-400 ${props.category ? "" : "text-gray-300"}`}
       >
-        {category ? "Category: " + category : ""}
+        {props.category ? "Category: " + props.category : ""}
       </h1>
 
       <h1 className="cursor-default text-sm text-gray-400">
-        {condition ? "Condition: " + ProductCondition[condition] : ""}
+        {props.condition === undefined
+          ? ""
+          : "Condition: " +
+            ProductCondition[props.condition]
+              .replace(/_/g, " ")
+              .toLowerCase()
+              .replace(/\b\w/g, (char) => char.toUpperCase())}
       </h1>
 
       <h1 className="cursor-default text-sm text-gray-400">
-        {color ? "Color: " + ProductColor[color] : ""}
+        {props.color === undefined || props.color === 0
+          ? ""
+          : "Color: " +
+            ProductColor[props.color]
+              .replace(/_/g, " ")
+              .toLowerCase()
+              .replace(/\b\w/g, (char) => char.toUpperCase())}
       </h1>
 
       <h1 className="cursor-default text-sm text-gray-400">
-        {year ? "Production year: " + year : ""}
+        {props.year ? "Production year: " + props.year : ""}
       </h1>
 
       <p
-        className={`my-3 cursor-default text-xl ${price ? "" : "text-gray-300"}`}
+        className={`my-3 cursor-default text-xl ${props.price ? "" : "text-gray-300"}`}
       >
-        {price ? price + " kr" : "No price"}
+        {props.price ? currencyFormat.format(props.price) : "No price"}
       </p>
 
       <h1 className="cursor-default text-sm">
-        {description ? "Description: " : ""}
+        {props.description ? "Description: " : ""}
       </h1>
       <p className="truncate text-wrap hover:text-clip mobile-br:text-sm">
-        {description ? description : ""}
+        {props.description ? props.description : ""}
       </p>
     </div>
   );
